ed424d9cb672b1590d7645110e19a85e1c2e21d7
akhilmhdh <akhilmhdh@gmail.com>
2022-08-09 22:02:06 +0530

    feat: added fade in animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
   mode: 'jit',
   theme: {
     extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+      },
       fontFamily: {
         lato: ['Lato', 'sans-serif'],
         merriweather: ['Merriweather', 'serif'],

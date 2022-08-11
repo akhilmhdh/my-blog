31b8b82699aa36a684d30f3bf5382454867a657b
akhilmhdh <akhilmhdh@gmail.com>
2022-08-11 22:16:49 +0530

    feat: made the design mobile responsive

--- src/_includes/js/buildSocialMediaURL.js
+++ src/_includes/js/buildSocialMediaURL.js
@@ -1,46 +1,52 @@
 // This will build the social media links for the blog
 // To share content easily
 
-window.onload = () => {
-  const title = document.querySelector('h1').innerText;
-  const url = window.location.href;
+const title = document.querySelector('h1').innerText;
+const url = window.location.href;
 
-  // facebook share url
-  const facebook = document.querySelector('#share-facebook');
-  if (facebook) {
-    facebook.setAttribute('href', 'https://facebook.com/sharer.php?u=' + url);
-  }
+// facebook share url
+const facebook = document.querySelectorAll('.share-facebook');
+if (facebook) {
+  facebook.forEach((node) =>
+    node.setAttribute('href', 'https://facebook.com/sharer.php?u=' + url)
+  );
+}
 
-  // mail share url
-  const mail = document.querySelector('#share-mail');
-  if (mail) {
-    mail.setAttribute(
+// mail share url
+const mail = document.querySelectorAll('.share-mail');
+if (mail) {
+  mail.forEach((node) =>
+    node.setAttribute(
       'href',
       'mailto:?subject=' +
         title +
         '&body=Check out this cool blog by akhilmhdh.com: ' +
         url
-    );
-  }
+    )
+  );
+}
 
-  //  twitter url
-  const twitter = document.querySelector('#share-twitter');
-  if (twitter) {
-    twitter.setAttribute(
+//  twitter url
+const twitter = document.querySelectorAll('.share-twitter');
+if (twitter) {
+  twitter.forEach((node) =>
+    node.setAttribute(
       'href',
       'https://twitter.com/share?url=' +
         url +
         '&text=' +
         title +
         ' from @akhilmhdh \n'
-    );
-  }
+    )
+  );
+}
 
-  const linkedin = document.querySelector('#share-linkedin');
-  if (linkedin) {
-    linkedin.setAttribute(
+const linkedin = document.querySelectorAll('.share-linkedin');
+if (linkedin) {
+  linkedin.forEach((node) =>
+    node.setAttribute(
       'href',
       'https://www.linkedin.com/sharing/share-offsite/?url=' + url
-    );
-  }
-};
+    )
+  );
+}

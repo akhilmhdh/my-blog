78121a9b0a8e88c5d014ad12ced1437e00393488
akhilmhdh <akhilmhdh@gmail.com>
2022-08-11 12:49:04 +0530

    feat: added smooth scroll and reading progress bar

---
+++ src/_includes/js/readingProgressbar.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  document.addEventListener('scroll', () => {
+    const docElem = document.documentElement;
+    const docBody = document.body;
+    const scrollTop = docElem['scrollTop'] || docBody['scrollTop'];
+    const scrollBottom =
+      (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight;
+    const scrollPercent = (scrollTop / scrollBottom) * 100 + '%';
+
+    document
+      .getElementById('progress-bar')
+      .style.setProperty('--scrollAmount', scrollPercent);
+  });
+};

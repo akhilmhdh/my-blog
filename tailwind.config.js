c95cc9323f0935a116b7fbf19cdbef09e8633733
akhilmhdh <akhilmhdh@gmail.com>
2022-09-01 23:30:42 +0530

    feat: updated logo, fixed alignment issues in UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      fontSize: {
+        xxs: '0.625rem',
+      },
       keyframes: {
         'fade-in-down': {
           '0%': {
@@ -16,9 +19,22 @@ module.exports = {
             transform: 'translateY(0)',
           },
         },
+        'bounce-short': {
+          '0%, 100% ': {
+            transform: 'translateY(-8%)',
+            '-webkit-animation-timing-function': 'cubic-bezier(0.8,0,1,1)',
+            'animation-timing-function': 'cubic-bezier(0.8,0,1,1)',
+          },
+          '50%': {
+            transform: 'none',
+            ' -webkit-animation-timing-function': 'cubic-bezier(0,0,0.2,1)',
+            'animation-timing-function': 'cubic-bezier(0,0,0.2,1)',
+          },
+        },
       },
       animation: {
         'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'bounce-short': 'bounce-short 1s ease-in-out 4',
       },
       fontFamily: {
         lato: ['Lato', 'sans-serif'],
